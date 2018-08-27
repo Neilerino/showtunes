386bc6356848a3a93c16342705e319bbb23aa2a1
Neil Wadden <neilwadden@pop-os.localdomain>
2018-08-27 17:20:41 -0230

    Got alot of front end started, and user login page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from flask import render_template, flash, redirect
+from flask_login import current_user, login_user
+from app.models import User
+from app.forms import LoginForm
 from app import app
 
 
@@ -6,3 +9,23 @@ from app import app
 @app.route('/home')
 def home():
 	return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect('/home')
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('home'))
+	return render_template('login.html', form=form)
+
+
+
+@app.route('/register')
+def register():
+	return render_template("register.html")
